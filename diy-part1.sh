b5544e4616398fd19dc5592a9f4767fb61a2e238
Chenmo <ofnhkb1@126.com>
2022-03-24 14:59:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,20 +29,21 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 mkdir -p package/custom
 cd package/custom
-git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+#git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial
+#微信推送
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
+#选择Network > smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns
-git clone --depth=1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
 git clone --depth=1 https://github.com/kenzok8/small
 
-
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 https://github.com/rosywrt/luci-theme-rosy.git
+#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone --depth=1 https://github.com/rosywrt/luci-theme-rosy.git
 git clone --depth=1 https://github.com/sypopo/luci-theme-argon-mc.git
-git clone --depth=1 https://github.com/apollo-ng/luci-theme-darkmatter.git
+#git clone --depth=1 https://github.com/apollo-ng/luci-theme-darkmatter.git
